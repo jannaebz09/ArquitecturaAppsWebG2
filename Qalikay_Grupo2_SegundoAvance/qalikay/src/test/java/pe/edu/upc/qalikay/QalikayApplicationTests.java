199888c30cf7163742f1301e392521837cf7aa83
Leonardo <113949255+LeonardoSicha@users.noreply.github.com>
2024-04-01 20:55:30 -0500

    Segunda Version
    Se añadio sección "Receta Especial"

---
+++ Qalikay_Grupo2_SegundoAvance/qalikay/src/test/java/pe/edu/upc/qalikay/QalikayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.qalikay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalikayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

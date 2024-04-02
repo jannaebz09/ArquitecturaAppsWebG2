bd33dc463faef6cde677f5da0b79dc1e0c095180
Leonardo <113949255+LeonardoSicha@users.noreply.github.com>
2024-04-01 21:00:31 -0500

    Delete Qalikay_Grupo2_SegundoAvance/qalikay directory

--- Qalikay_Grupo2_SegundoAvance/qalikay/src/main/java/pe/edu/upc/qalikay/repositories/IClientRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.qalikay.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.qalikay.entities.Client;
-
-@Repository
-public interface IClientRepository extends JpaRepository<Client, Integer> {
-}
--- Qalikay_Grupo2_SegundoAvance/qalikay/src/main/java/pe/edu/upc/qalikay/servicesinterfaces/ISp_recipeService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.qalikay.servicesinterfaces;
-
-import pe.edu.upc.qalikay.entities.Sp_recipe;
-
-import java.util.List;
-public interface ISp_recipeService {
-    public void insert (Sp_recipe sp_recipe);
-    public List<Sp_recipe> list();
-}
